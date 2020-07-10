faea38fd513470686d0a9c0794a237452fb63280
sungkuk5420 <sungkuk5420@gmail.com>
2020-07-11 01:50:17 +0900

    Adding the Icons

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,8 +1,10 @@
 import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { TabBarIOS } from 'react-native';
+import { Platform } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
+
 import Movies from '../screens/Movies';
-import Tv from '../screens/Tv';
+import TV from '../screens/Tv';
 import Search from '../screens/Search';
 import Favs from '../screens/Favs';
 
@@ -20,9 +22,38 @@ export default (props) => {
     }, [route]);
 
     return (
-        <Tabs.Navigator>
+        <Tabs.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused }) => {
+                    let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+                    if (route.name === "Movies") {
+                        iconName += "film";
+                    } else if (route.name === "TV") {
+                        iconName += "tv";
+                    } else if (route.name === "Search") {
+                        iconName += "search";
+                    } else if (route.name === "Favourites") {
+                        iconName += "heart";
+                    }
+                    return (
+                        <Ionicons
+                            name={iconName}
+                            color={focused ? "white" : "grey"}
+                            size={26}
+                        />
+                    );
+                }
+            })}
+            tabBarOptions={{
+                showLabel: false,
+                style: {
+                    backgroundColor: "black",
+                    borderTopColor: "black",
+
+                }
+            }}>
             <Tabs.Screen name="Movies" component={Movies} />
-            <Tabs.Screen name="Tv" component={Tv} />
+            <Tabs.Screen name="TV" component={TV} />
             <Tabs.Screen name="Search" component={Search} />
             <Tabs.Screen name="Favs" component={Favs} />
         </Tabs.Navigator>
