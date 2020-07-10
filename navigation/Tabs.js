7439f07d38d1cbdc971d72a664888411b0f7db00
sungkuk5420 <sungkuk5420@gmail.com>
2020-07-11 01:48:08 +0900

    Updating Header

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { TabBarIOS } from 'react-native';
 import Movies from '../screens/Movies';
@@ -7,12 +7,21 @@ import Search from '../screens/Search';
 import Favs from '../screens/Favs';
 
 const Tabs = createBottomTabNavigator();
+const getHeaderName = (route) => route?.state?.routeNames[route.state.index] || "Movies";;
+export default (props) => {
+    const navigation = props.navigation;
+    const route = props.route;
+    //useEffect(() => {
+    useLayoutEffect(() => {// layoutEffect는 user Effect랑 다른점이 레이아웃 변경이 다 끝난후에 작동함
+        navigation.setOptions({ title: getHeaderName(route) })
+    }, [route]);
 
-export default () => (
-    <Tabs.Navigator>
-        <Tabs.Screen name="Movies" component={Movies} />
-        <Tabs.Screen name="Tv" component={Tv} />
-        <Tabs.Screen name="Search" component={Search} />
-        <Tabs.Screen name="Favs" component={Favs} />
-    </Tabs.Navigator>
-)
+    return (
+        <Tabs.Navigator>
+            <Tabs.Screen name="Movies" component={Movies} />
+            <Tabs.Screen name="Tv" component={Tv} />
+            <Tabs.Screen name="Search" component={Search} />
+            <Tabs.Screen name="Favs" component={Favs} />
+        </Tabs.Navigator>
+    )
+}
